c42755290a446d97d7e72695e848881011da5c81
Gabriel Mena <117972132+Gmenaa@users.noreply.github.com>
2024-06-06 11:48:05 -0500

    Rough outline for home page, not logged in and logged in included.

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,8 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
 
 import axios from 'axios';
+
+import './Home.css';
 
 function Home () {
   const [auth, setAuth] = useState(false);
@@ -35,19 +37,57 @@ function Home () {
     <div className='home-container'>
       {
         auth ?
-        <div>
-          <h3>You are authorized --- {name}</h3>
-          <button className='btn-logout' onClick={handleDelete}>Logout</button>
+        <div className='hero-container'>
+            <header className='hero'>
+                <nav className='navbar'>
+                    <div className='left-nav'>
+                        <span>LOGO</span>
+                        <span>Home</span>
+                        <span>Features</span>
+                        <span>How it Works</span>
+                        <span>About us</span>
+                        <span>Contact</span>
+                    </div>
+                    <div className='right-nav'>
+                        <span>Capsules</span>
+                        <span>{name}</span>
+                        <button className='btn-logout' onClick={handleDelete}>Logout</button>
+                    </div>
+                </nav>
+                <div className= 'hero-text'>
+                    <h1>PRESERVE FOR THE FUTURE</h1>
+                    <h3>Create personal or shared time capsules, fill them with multimedia content, and unlock them on your chosen date</h3>
+                </div>
+            </header>
         </div>
         :
-        <div>
-          <h3>{message}</h3>
-          <h3>Login now</h3>
-          <Link to="/login" className='btn-login'>Login</Link>
+        <div className='hero-container'>
+            <header className='hero'>
+                <nav className='navbar'>
+                    <div className='left-nav'>
+                        <span>LOGO</span>
+                        <span>Home</span>
+                        <span>Features</span>
+                        <span>How it Works</span>
+                        <span>About us</span>
+                        <span>Contact</span>
+                    </div>
+                    <div className='right-nav'>
+                        <Link to="/login" className='btn-login'>Login</Link>
+                        <Link to="/register" className='btn-register'>Register</Link>
+                        <span>Capsules</span>
+                    </div>
+                </nav>
+                <div className= 'hero-text'>
+                    <h1>PRESERVE FOR THE FUTURE</h1>
+                    <h3>Create personal or shared time capsules, fill them with multimedia content, and unlock them on your chosen date</h3>
+                </div>
+            </header>
         </div>
+
       }
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home	
