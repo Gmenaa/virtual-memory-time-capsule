71fa4555e690492034a80f09c8a80e5b9ae164bb
Gabriel Mena <117972132+Gmenaa@users.noreply.github.com>
2024-07-23 18:21:48 -0500

    completed home section, buttons, and background header CSS. TODO: about section, fix DOM incompatabilities.

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -7,8 +7,11 @@ import AnchorLink from 'react-anchor-link-smooth-scroll'
 import 'remixicon/fonts/remixicon.css'
 
 // --========== CSS ==========--
-// import './Home.css';
 import './assets/css/Home.css';
+
+// --========== IMAGES ==========--
+import image_1 from './assets/img/temp.png'
+import image_2 from './assets/img/temp.png'
 
 // --========== MAIN JS ==========--
 import './assets/js/Home.js'
@@ -41,9 +44,11 @@ function Home () {
   }
 
   return (
-    <div className='home-container'>
+    // TODO: add parent-container-2
+    <div className='parent-container'>
       {
         auth ?
+        <div className='parent-container-2'>
           <header className='header' id='header'>
             <nav className='nav container'>
               <a href="#" className='nav__logo'>Capsules</a>
@@ -76,7 +81,67 @@ function Home () {
               </div>
             </nav>
           </header>
+
+          <main>
+            <section className='home' id='home'>
+              <img src={image_1} alt="temporary image" className='home__img' />
+
+              <div className='home__container container grid'>
+                <div className='home__data'>
+                  <span className='home__data-subtitle'>Discover the Forgotten</span>
+                  {/* <h1 className='home__data-title'>Explore the <br> Best <b>Beautiful  <br> Beaches</b></h1> */}
+                  <h1 className='home__data-title'>Remember Those we Respect</h1>
+                  <AnchorLink className='button' href="#">Explore</AnchorLink>
+                </div>
+
+                <div className='home__social'>
+                  <a target='_blank' className='home__social-link' href="https://www.facebook.com/">
+                    <i className='ri-facebook-box-fill'></i>
+                  </a>
+
+                  <a target='_blank' className='home__social-link' href="https://www.instagram.com/">
+                    <i className='ri-instagram-fill'></i>
+                  </a>
+
+                  <a target='_blank' className='home__social-link' href="https://www.twitter.com/">
+                    <i className='ri-twitter-fill'></i>
+                  </a>
+                </div>
+
+                <div className='home__info'>
+                  <div>
+                    <span className='home__info-title'>5 Media Forms</span>
+                    <a href="#" className="button button--flex button--link home__info-button">More <i className='ri-arrow-right-line'></i></a>
+                  </div>
+
+                  <div className='home__info-overlay'>
+                    <img src={image_2} alt="temporary image" className='home__info-img' />
+                  </div>
+                </div>
+              </div>
+            </section>
+
+            <section className='about section' id='about'>
+              
+            </section>
+
+            <section className='discover section' id='discover'>
+              
+            </section>
+
+            <section>
+              
+            </section>
+          </main>
+        </div>
+
+          
+
         :
+
+
+
+        <div className='parent-container-2'>
           <header className='header' id='header'>
             <nav className='nav container'>
               <a href="#" className='nav__logo'>Capsules</a>
@@ -108,6 +173,59 @@ function Home () {
               </div>
             </nav>
           </header>
+
+          <main>
+            <section className='home' id='home'>
+              <img src={image_1} alt="temporary image" className='home__img' />
+
+              <div className='home__container container grid'>
+                <div className='home__data'>
+                  <span className='home__data-subtitle'>Discover the Forgotten</span>
+                  {/* <h1 className='home__data-title'>Explore the <br> Best <b>Beautiful  <br> Beaches</b></h1> */}
+                  <h1 className='home__data-title'>Remember Those we Respect</h1>
+                  <AnchorLink className='button' href="#">Explore</AnchorLink>
+                </div>
+
+                <div className='home__social'>
+                  <a target='_blank' className='home__social-link' href="https://www.facebook.com/">
+                    <i className='ri-facebook-box-fill'></i>
+                  </a>
+
+                  <a target='_blank' className='home__social-link' href="https://www.instagram.com/">
+                    <i className='ri-instagram-fill'></i>
+                  </a>
+
+                  <a target='_blank' className='home__social-link' href="https://www.twitter.com/">
+                    <i className='ri-twitter-fill'></i>
+                  </a>
+                </div>
+
+                <div className='home__info'>
+                  <div>
+                    <span className='home__info-title'>5 Media Forms</span>
+                    <a href="#" className="button button--flex button--link home__info-button">More <i className='ri-arrow-right-line'></i></a>
+                  </div>
+
+                  <div className='home__info-overlay'>
+                    <img src={image_2} alt="temporary image" className='home__info-img' />
+                  </div>
+                </div>
+              </div>
+            </section>
+
+            <section className='about section' id='about'>
+              
+            </section>
+
+            <section className='discover section' id='discover'>
+              
+            </section>
+
+            <section>
+              
+            </section>
+          </main>
+        </div>
       }
     </div>
   );
