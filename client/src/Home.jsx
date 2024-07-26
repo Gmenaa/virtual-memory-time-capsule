e210347670afa6b89ddc067163b85303103f0dcf
Gabriel Mena <117972132+Gmenaa@users.noreply.github.com>
2024-07-25 20:20:18 -0500

    TODO: Fix DOM issuegit add .! Impeading on video player and page load, Also, Fix SwiperJS Issue. Completed experience section and tour.

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -6,6 +6,19 @@ import AnchorLink from 'react-anchor-link-smooth-scroll'
 // --========== REMIX ICONS ==========--
 import 'remixicon/fonts/remixicon.css'
 
+// TODO:
+// --========== SWIPER ==========--
+// import './assets/css/swiper-bundle.min.css'
+// import './assets/js/swiper-bundle.min.js'
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { EffectCoverflow, Pagination } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+
+
 // --========== CSS ==========--
 import './assets/css/Home.css';
 
@@ -15,6 +28,13 @@ import image_2 from './assets/img/temp.png'
 
 import image_3 from './assets/img/temp.png'
 import image_4 from './assets/img/temp.png'
+
+import image_5 from './assets/img/temp.png'
+import image_6 from './assets/img/temp.png'
+
+import video_1 from './assets/video/stock.mov'
+// import image_7 from './assets/img/temp.png'
+// import image_8 from './assets/img/temp.png'
 
 // --========== MAIN JS ==========--
 import './assets/js/Home.js'
@@ -47,11 +67,12 @@ function Home () {
   }
 
   return (
-    // TODO: add parent-container-2
+
+      
     <div className='parent-container'>
       {
         auth ?
-        <div className='parent-container-2'>
+        <>
           <header className='header' id='header'>
             <nav className='nav container'>
               <a href="#" className='nav__logo'>Capsules</a>
@@ -150,14 +171,103 @@ function Home () {
             </section>
 
             <section className='discover section' id='discover'>
-              
+            <h2 className="section__title">Discover the most <b> Precious Memories</b></h2>
+              <div className="discover__container container ">
+                  <Swiper effect={'coverflow'} grabCursor={true} center edSlides={true} slidesPerView={'auto'} coverflowEffect={{rotate: 50, stretch: 0, depth: 100, modifier: 1, slideShadows: true,}} pagination={true} modules={[EffectCoverflow, Pagination]} className="mySwiper">
+                      {/* <!--==================== DISCOVER 1 ====================--> */}
+                      <SwiperSlide className="discover__card ">
+                          <img src="https://swiperjs.com/demos/images/nature-1.jpg" alt="" className="discover__img" />
+                          <div className="discover__data">
+                              <h2 className="discover__title">Ants</h2>
+                              <span className="discover__description">Lorem</span>
+                          </div>
+                      </SwiperSlide>
+
+                      {/* <!--==================== DISCOVER 2 ====================--> */}
+                      <SwiperSlide className="discover__card ">
+                          <img src="https://swiperjs.com/demos/images/nature-2.jpg" alt="" className="discover__img" />
+                          <div className="discover__data">
+                              <h2 className="discover__title">Sun</h2>
+                              <span className="discover__description">Lorem</span>
+                          </div>
+                      </SwiperSlide>
+
+                      {/* <!--==================== DISCOVER 3 ====================--> */}
+                      <SwiperSlide className="discover__card ">
+                          <img src="https://swiperjs.com/demos/images/nature-3.jpg" alt="" className="discover__img" />
+                          <div className="discover__data">
+                              <h2 className="discover__title">Sea</h2>
+                              <span className="discover__description">Lorem</span>
+                          </div>
+                      </SwiperSlide>
+
+                      {/* <!--==================== DISCOVER 4 ====================--> */}
+                      <SwiperSlide className="discover__card ">
+                          <img src="https://swiperjs.com/demos/images/nature-4.jpg" alt="" className="discover__img" />
+                          <div className="discover__data">
+                              <h2 className="discover__title">Forest</h2>
+                              <span className="discover__description">Lorem</span>
+                          </div>
+                      </SwiperSlide>
+                  </Swiper>
+              </div>
             </section>
 
-            <section>
-              
+            <section className="experience section">
+              <h2 className="section__title">With Our Experience <b> We Will Serve You </b></h2>
+
+              <div className="experience__container container grid">
+                <div className="experience__content grid">
+                  <div className="experience__data">
+                    <h2 className="experience__number">20</h2>
+                    <span className="experience__description">Year <b> Experience </b></span>
+                  </div>
+
+                  <div className="experience__data">
+                    <h2 className="experience__number">75</h2>
+                    <span className="experience__description">Complete <b> tours </b></span>
+                  </div>
+
+                  <div className="experience__data">
+                    <h2 className="experience__number">650+</h2>
+                    <span className="experience__description">Tourist <b> Destination </b></span>
+                  </div>
+                </div>
+
+                <div className="experience__img grid">
+                  <div className="experience__overlay">
+                    <img src={image_5} alt="" className="experience__img-one" />
+                  </div>
+                    
+                  <div className="experience__overlay">
+                    <img src={image_6} alt="" className="experience__img-two" />
+                  </div>
+                </div>
+              </div>
+            </section>
+
+
+            <section className="video section">
+                <h2 className="section__title">Video Tour</h2>
+
+                <div className="video__container container">
+                    <p className="video__description">Find out more with our video of the most beautiful and 
+                        pleasant places for you and your family.
+                    </p>
+
+                    <div className="video__content">
+                        <video id="video-file">
+                            <source src={video_1} type="video/mp4" />
+                        </video>
+
+                        <button className="button button--flex video__button" id="video-button">
+                            <i className="ri-play-line video__button-icon" id="video-icon"></i>
+                        </button>
+                    </div>
+                </div>
             </section>
           </main>
-        </div>
+        </>
 
           
 
@@ -165,7 +275,7 @@ function Home () {
 
 
 
-        <div className='parent-container-2'>
+        <>
           <header className='header' id='header'>
             <nav className='nav container'>
               <a href="#" className='nav__logo'>Capsules</a>
@@ -271,7 +381,7 @@ function Home () {
               
             </section>
           </main>
-        </div>
+        </>
       }
     </div>
   );
